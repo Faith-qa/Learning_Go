17eb459cd5cd3e228dd30b916d4e17738210452c
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2023-04-07 16:41:16 +0300

    initial setup

---
+++ movies-rest_api/main.go
@@ -0,0 +1,12 @@
+package main
+
+type movie struct {
+	ID       string    `json: "id"`
+	Isbn     string    `json: "isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json: "lastname"`
+}
