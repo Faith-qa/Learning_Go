c490a50ac44b79c6062d742fd06e617ed5739074
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2023-04-07 11:37:20 +0300

    a simple go server

--- go-server/README.md
+++ go-server/README.md
@@ -1,4 +1,11 @@
-# Go server
+# Simple Go server
 
-A simple Crud http server to help my understanding of go
-s
+This is a simple Go server that serves a simple http form that takes in the name and address.
+
+## Key learning objcetive:
+* setting up a go project
+* creating functions in go
+* how to use the `fmt, log, http` modules
+* running and compiling a go project
+* creating a `go module` 
+* how to use `go build` and `go run <filename>`
--- go-server/main.go
+++ go-server/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request seccessful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+
+// prints hello how are you on the screen
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method.found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello how are you")
+}
+
 func main() {
+	//directing go to the static index.html files
+	fileserver := http.FileServer(http.Dir("./static"))
+
+	//create routes
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	//prompt to inform user that the server is starting
+	fmt.Printf("starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
