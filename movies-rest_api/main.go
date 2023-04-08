b4bf7b916130141981dbb6b18b804b41e2b5cdf0
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2023-04-08 07:23:01 +0300

    created the createmovie POST function

--- movies-rest_api/main.go
+++ movies-rest_api/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -59,6 +61,19 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+// create a movie function
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000000))
+
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -66,7 +81,7 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Isbn: "978654", Title: "Merlin", Director: &Director{Firstname: "nelly", Lastname: "Faith"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	//r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
 	//r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
