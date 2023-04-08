56a306fdfd7d143a8683332fe4daefb664d4a47e
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2023-04-08 06:01:40 +0300

    created a get request to get all movies

--- movies-rest_api/main.go
+++ movies-rest_api/main.go
@@ -1,6 +1,15 @@
 package main
 
-type movie struct {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"githhub.com/gorilla/mux"
+)
+
+type Movie struct {
 	ID       string    `json: "id"`
 	Isbn     string    `json: "isbn"`
 	Title    string    `json:"title"`
@@ -9,4 +18,28 @@ type movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json: "lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "234567", Title: "The fleek", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "978654", Title: "Merlin", Director: &Director{Firstname: "nelly", Lastname: "Faith"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	//r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	//r.HandleFunc("/movies", createMovie).Methods("POST")
+	//r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	//r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	//start server
+	fmt.Printf("Starting server at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
