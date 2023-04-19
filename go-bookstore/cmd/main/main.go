201bf2cc8164dbb576c8eaaef95531a14ed043e6
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2023-04-19 06:04:18 +0300

    created the server

---
+++ go-bookstore/cmd/main/main.go
@@ -0,0 +1,19 @@
+// create the server
+package main
+
+import (
+	"go-bookstore/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost: 9010", r))
+
+}
