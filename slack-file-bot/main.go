a4aa8a0e0bd514e35373c5e799c7bf3d0087c1ca
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2023-04-25 15:33:49 +0300

    created main function to handle file upload

--- slack-file-bot/main.go
+++ slack-file-bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -23,5 +24,20 @@ func main() {
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 	channelArr := []string{os.Getenv("CHANNEL_ID")}
 	fileArr := []string{""}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+
+	}
 
 }
